1ada7713e7563cd59a0eb2bad9f9cf419b5bc507
Gaber Ahmed <145846033+gabr18431@users.noreply.github.com>
2024-02-29 22:00:39 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,18 +1,24 @@
-// sliders  
-const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    // direction: 'vertical',
-    loop: true,
-  
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true, // تفعيل النقر على ال pagination
-    },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  });
+// sliders
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  // direction: 'vertical',
+  loop: true,
+  autoplay: {
+    delay: 4000, // تحديد فترة التوقف بين كل انتقال (بالميلي ثانية)
+    disableOnInteraction: false, // يمنع تعطيل التمرير التلقائي بمجرد تفاعل المستخدم (افتراضيًا true)
+  },
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true, // تفعيل النقر على ال pagination
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+console.log('tessssssss');
