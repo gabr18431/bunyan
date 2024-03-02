d1ec636c76dc88047a4686687fd52977c4ad53e4
Gaber Ahmed <145846033+gabr18431@users.noreply.github.com>
2024-03-02 19:23:21 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -13,3 +13,21 @@ async function handelClickSystemItem(item) {
   currentSystem.classList.toggle('show');
 }
 
+// mobile-menu ****************************
+
+let mobilMenu = document.querySelector('.mobile-menu');
+let header = document.querySelector('header');
+let closeIcon = document.querySelector('.close-icon');
+let overlay = document.querySelector('.overlay');
+
+mobilMenu.addEventListener('click', () => {
+  header.classList.add('show-menu');
+})
+
+closeIcon.addEventListener('click', () => {
+  header.classList.remove('show-menu');
+})
+
+overlay.addEventListener('click', () => {
+  header.classList.remove('show-menu');
+})
